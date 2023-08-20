b043ac21354450e56b1d43c86b145cce4ac37a88
Hugo Montoya Vazquez <hmontoyavazquez@gmail.com>
2023-08-20 18:01:55 +0200

    messing with get line

--- get_next_line.c
+++ get_next_line.c
@@ -4,23 +4,25 @@
 
 char *get_next_line(int fd)
 {
-	static char buffer[1000];
+	static char *buffer;
+	char	*chunk;
 	ssize_t byte;
+	ssize_t i;
+	char *line;
 
-	byte = read(fd, buffer, 1000);
-	//buffer = (char *)malloc(byte * sizeof(char *));
-	/*if (!buffer)
-	{
-		printf(":: Error: memory allocation failed. (0 bytes from %zu)\n",byte);
-		return (NULL);
-	}*/
+	chunk = malloc(BUFFER_SIZE * sizeof(char *));
+	byte = read(fd, chunk, BUFFER_SIZE);
+	i = 0;
 	printf(":: Number of bytes allocated %zu\n", byte);
-	while (byte > 0)
+	while (bytes > 0 && buffer[i] != '\n')
 	{
-		printf(">> Buffer (%zu bytes)\n\n", byte);
-		byte += read(fd, buffer, 1000 * byte);
-		printf("%c\n", buffer[byte]);
-		return (buffer);
+		printf(">> Buffer (%zu bytes)\n\n", i);
+		if (i = ft_strchar(buffer,'\n'))
+		{
+			
+		}
+		i++;	
+		printf("%c\n", buffer[i]);
 	}
      return (buffer);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -4,5 +4,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE 100
+
 char *get_next_line(int fd);
 
